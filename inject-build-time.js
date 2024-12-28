a79d8a8be48461712e8d1386705e4df5624335c5
Andrew Shen <39780709+andrew-shc@users.noreply.github.com>
2024-12-28 01:54:06 +0000

    fix last update time; better handling on resize

---
+++ inject-build-time.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+// Get the current build time
+const buildTime = new Date().toLocaleString();
+
+// Replace the placeholder with the actual build time
+fs.readFile('./index.html', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading HTML file:', err);
+    return;
+  }
+
+  const updatedHTML = data.replace('BUILD_TIME_PLACEHOLDER', buildTime);
+
+  fs.writeFile(filePath, updatedHTML, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing updated HTML file:', err);
+    } else {
+      console.log('Build time successfully injected!');
+    }
+  });
+});
